9fa506686ffcf42a1f548794761677f2e365f2bb
Marcelo Armando <46385599+marcelcr98@users.noreply.github.com>
2020-07-07 23:45:26 -0500

    api xamarin

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'us-cdbr-iron-east-01.cleardb.net',
+    host: ' ',
     port:3306,
-    user: 'bda87bde2321a4',
-    password: '51ad6e9a',
-    database: 'heroku_df2f4a10ff3ea9d'
+    user: ' ',
+    password: ' ',
+    database: ' '
 })
 
 connection.connect(function(err) {
