8981ecb0ed3ceee0e02ce1a233c1097f3b8ce0f3
marceloarmando <mcuellarr98@gmail.com>
2020-07-13 13:46:32 -0500

    commit 7

--- models/db.js
+++ models/db.js
@@ -1,15 +1,12 @@
 var mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'bda87bde2321a4',
     password: '51ad6e9a',
     database: 'heroku_df2f4a10ff3ea9d'
 })
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("mysql connected");
-})
+
 
 module.exports = connection;
