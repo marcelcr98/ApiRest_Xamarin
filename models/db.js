1ca42bb70896e82ff8b09018034dbc991a3d916e
marceloarmando <mcuellarr98@gmail.com>
2020-07-13 12:49:40 -0500

    commit 2

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: ' ',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
     port:3306,
-    user: ' ',
-    password: ' ',
-    database: ' '
+    user: 'bda87bde2321a4',
+    password: '51ad6e9a',
+    database: 'heroku_df2f4a10ff3ea9d'
 })
 
 connection.connect(function(err) {
